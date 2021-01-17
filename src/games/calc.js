11059202b129f5eea4437e4fcaf66e053960b4bd
Sergey Demikhov <demikhov.sergey@gmail.com>
2021-01-17 22:05:43 +0400

    fix style

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,14 +5,13 @@ const operator = { '+': _.add, '-': _.subtract, '*': _.multiply };
 export default () => ({
   description: 'What is the result of the expression?',
   generateQuiz: () => {
-    const sign = _.sample(Object.keys(operator));
+    const operation = _.sample(Object.keys(operator));
 
     const operand1 = _.random(100);
     const operand2 = _.random(100);
 
-    const question = `${operand1} ${sign} ${operand2}`;
-    const calculateExpression = operator[sign];
-    const answer = String(calculateExpression(operand1, operand2));
+    const question = `${operand1} ${operation} ${operand2}`;
+    const answer = String(operator[operation](operand1, operand2));
 
     return { question, answer };
   },
